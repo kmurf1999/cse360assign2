053b9749ea32b19330381c6b69ddd5c634ea6b11
Kyle Murphy <kmurf1999@gmail.com>
2019-10-09 22:23:27 -0700

    second commit

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,4 +1,6 @@
 /**
+ * Assignement 2
+ * 
  * @author Kyle Murphy <kwmurph2@asu.edu>
  * @date 10/09/2019
  */
@@ -8,6 +10,7 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String transactions;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
@@ -15,24 +18,31 @@ public class AddingMachine {
 	
 	// return total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	// update transaction string
 	// add to total
 	public void add (int value) {
-		
+		transactions += " + " + Integer.toString(value);
+		total += value;
 	}
 	
+	// update transaction string
 	// subtract from total
 	public void subtract (int value) {
-		
+		transactions += " - " + Integer.toString(value);
+		total -= value;
 	}
-		
+	
+	// return transaction record string	
 	public String toString () {
-		return "";
+		return transactions;
 	}
 
+	// reset private data
 	public void clear() {
-	
+		total = 0;
+		transactions = "0";
 	}
 }
