99160e3de5ef463b3d3731cadc4bd3b36ef02b1b
Kyle Murphy <kmurf1999@gmail.com>
2019-10-09 22:02:58 -0700

    First commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,38 @@
+/**
+ * @author Kyle Murphy <kwmurph2@asu.edu>
+ * @date 10/09/2019
+ */
+package cse360assign2;
+
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	// return total
+	public int getTotal () {
+		return 0;
+	}
+	
+	// add to total
+	public void add (int value) {
+		
+	}
+	
+	// subtract from total
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
